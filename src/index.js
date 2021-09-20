d15c8b57607cf7d4f08639de2491337a9bf8c092
Manuela Rios <yorios9312@gmail.com>
2021-09-19 20:05:06 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -181,7 +181,11 @@ app.use((error, request, response, next) => {
   })
 })
 
-const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log('Running: http://localhost:' + port)
-})
+// const port = process.env.PORT || 5000
+// app.listen(port, () => {
+//   console.log('Running: http://localhost:' + port)
+// })
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
