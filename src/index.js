96eb8c95dfe1b24964fc709350584bb447b5a902
Manuela Rios <yorios9312@gmail.com>
2021-09-19 20:07:26 -0700

    Update index.js
    Change the port back

--- src/index.js
+++ src/index.js
@@ -181,11 +181,7 @@ app.use((error, request, response, next) => {
   })
 })
 
-// const port = process.env.PORT || 5000
-// app.listen(port, () => {
-//   console.log('Running: http://localhost:' + port)
-// })
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log('Running: http://localhost:' + port)
+})
